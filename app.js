4b860268c4776d4c16329e28e84a16e70eec2ec8
Ande Kishore <120158003+VijayGovind123@users.noreply.github.com>
2023-08-07 18:30:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -154,6 +154,73 @@ app.post("/coordinates",function(req,res){
 });
 
 
+app.get("/location", function (req, res) {
+  res.sendFile(__dirname + "/location.html");
+});
+app.post("/location", function (req, res) {
+  console.log(req.body);
+  const lat = req.body.latitude;
+  const long = req.body.longitude;
+  const appId = "0f871056c1dab081a447965f8702e88a";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    long +
+    "&appid=" +
+    appId +
+    "&units=metric";
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+
+      const status = weatherData.cod;
+      if (status == 400 || status == 404) {
+        res.render("failure", { page: "Co-ordinates" });
+      }
+      if (status != 400) {
+        var temp = weatherData.main.temp;
+
+        const minTemp = weatherData.main.temp_min;
+
+        const maxTemp = weatherData.main.temp_max;
+
+        const weatherDescription = weatherData.weather[0].description;
+
+        const humidity = weatherData.main.humidity;
+
+        const pressure = weatherData.main.pressure;
+
+        const windSpeed = weatherData.wind.speed;
+
+        const imgUrl =
+          "http://openweathermap.org/img/wn/" +
+          weatherData.weather[0].icon +
+          "@2x.png";
+        const cityName = weatherData.name;
+        const country = weatherData.sys.country;
+        const condition = weatherData.weather[0].main;
+
+        res.render("index", {
+          currentTemp: temp,
+          ImgUrl: imgUrl,
+          weatherDescription: weatherDescription,
+          countryCode: country,
+          cityName: cityName,
+          maxTemp: maxTemp,
+          minTemp: minTemp,
+          humidity: humidity,
+          pressure: pressure,
+          windSpeed: windSpeed,
+          currentTime: currentTime,
+          condition: condition,
+        });
+      }
+    });
+  });
+});
+
+
 
 
 
